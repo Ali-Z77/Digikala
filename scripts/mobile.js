0ec336a92536f57418be51ad2af59552b4cf7c6a
Ali-Z77 <Ali.zarei.9877@gmail.com>
2023-08-03 22:47:26 +0330

    generate mobile.html

---
+++ scripts/mobile.js
@@ -0,0 +1,39 @@
+import { products } from "../data/products.js";
+
+let mobileHtml = ''
+
+products.forEach((product) => {
+  mobileHtml += `
+    <div class="product-container">
+      <div class="image-container">
+        <img class="product-image" src="${product.image}">
+      </div>
+      <div>
+        <div class="text-1">
+          <div class="text-1-1">
+            <div><img class="red-truck" src="images/red-truck.png"></div>
+            <div><span>ارسال رایگان</span></div>
+          </div>
+        </div>
+        <div class="text-2">
+          <h3>${product.name}</h3>
+        </div>
+        <div class="text-3">
+          <div class="emission">
+            <img class="fast-emission-icon" src="images/fast-emission.png">
+            <span>ارسال فردا</span>
+          </div>
+          <div class="score">
+            <div>${product.starNumber}</div>
+            <div><img src="images/star-yellow.png"></div>
+          </div>
+        </div>
+        <div class="text-4">
+          <span>${product.price}</span>
+          <span id="tooman">تومان</span>
+        </div>
+      </div>
+    </div>`
+});
+
+document.querySelector('.container').innerHTML = mobileHtml;
